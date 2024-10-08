b6d6c0aae7d3f61bc53256f0c13e45293c728d63
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-10-08 21:13:29 +0530

    printing

---
+++ 05_JAVASCRIPT/udemy/01_printing.js
@@ -0,0 +1,5 @@
+console.log("Jai Shree Ram - 1");
+process.stdout.write("Jai Shree Ram - 2"); // nodejs built-in function
+console.warn("Jai Shree Ram - 3");
+console.error("Jai Shree Ram - 4");
+console.table({ sansaar: "Jai Shree Ram - 5" });
