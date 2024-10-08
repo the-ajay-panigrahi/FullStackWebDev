58bc6d1c12d2b460a27be02a1f4bb07483be124e
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-10-08 22:35:16 +0530

    changes

---
+++ 05_JAVASCRIPT/udemy/03_changes.js
@@ -0,0 +1,11 @@
+let gameName = "Spiderman";
+console.log(gameName);
+
+gameName = "Batman";
+console.log(gameName);
+
+const PI = 3.14;
+console.log(PI);
+
+// PI = 3; // TypeError: Assignment to constant variable
+// console.log(PI);
