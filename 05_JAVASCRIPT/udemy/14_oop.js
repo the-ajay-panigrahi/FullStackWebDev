40cafe57ce36bb5be461999135eae39760c72303
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-10-16 15:59:23 +0530

    Encapsulation

--- 05_JAVASCRIPT/udemy/14_oop.js
+++ 05_JAVASCRIPT/udemy/14_oop.js
@@ -5,9 +5,10 @@
  Classes - Classes are blueprints to create objects. They're just syntactic sugar, under the hood, it's all based on prototypes. In JavaScript, everything is an object, so even classes themselves are objects under the hood.
 
  Inheritance - Inheritance means borrowing functionality from another class. If my dad owns a car, I can borrow it whenever I need. Similarly, in inheritance, I can permanently use functionality from another class.
+
+ Encapsulation - Encapsulation is the process of hiding internal details and exposing only the necessary functionality. It restricts direct access to an object's data to protect it from outside interference.
  
  */
-
 
 /* 
     When using a regular JavaScript object like let obj = {} or the below, we can't easily create multiple copies. This issue is solved by using constructor functions.
@@ -68,3 +69,23 @@ let doggy = new Dog("German Shepherd", "Buddy", 2);
 // console.log(doggy);
 // console.log(doggy.speak());
 // console.log(doggy.sound());
+
+// Encapsulation
+class BankAccount {
+  #balance = 12345; // private variable
+
+  deposit(amount) {
+    this.#balance += amount;
+  }
+
+  getBalance() {
+    return `$${this.#balance}`;
+  }
+}
+
+let account = new BankAccount();
+// console.log(account.balance); // undefined
+// console.log(account.#balance); // not accessible outside the class
+// console.log(account.getBalance());
+// account.deposit(12345);
+// console.log(account.getBalance());
