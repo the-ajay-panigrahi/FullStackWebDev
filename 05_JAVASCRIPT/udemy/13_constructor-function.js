3de2e366976a140d13521830e85460e872110be6
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-10-16 14:27:25 +0530

    prototypal chaining

--- 05_JAVASCRIPT/udemy/13_constructor-function.js
+++ 05_JAVASCRIPT/udemy/13_constructor-function.js
@@ -4,6 +4,7 @@
     3) this: Refers to the current context or object.
     4) throw: Used to throw a custom error or exception.
     5) Error(): The constructor to create a new error object.
+    6) Prototypal chaining - Prototypal chaining is like a chain of objects where each object can borrow or use properties and methods from its prototype (parent object). Prototypal chaining is a feature in JavaScript where objects can inherit properties and methods from other objects.
 */
 
 function Person(name, age) {
@@ -17,7 +18,7 @@ function Person(name, age) {
   };
 }
 
-let person_0 = Person("Ajay", 20);
+// let person_0 = Person("Ajay", 20);
 // console.log(person_0);
 
 let person_1 = new Person();
@@ -31,8 +32,25 @@ let person_3 = new Person("Ruchika", 16);
 // console.log(person_3);
 // console.log(person_3.describe);
 
+// The below thing is called prototypal chaining
 Person.prototype.sound = function () {
   return `${this.name} makes a lot of sound!!`;
 };
 // console.log(person_2.sound());
 // console.log(person_3.sound());
+
+// Another example of prototypal chaining
+Array.prototype.sayHelloToAjay = function () {
+  return `Hello Ajay!!, how are you? ${this}`;
+};
+let arr = [1, 2, 3, 4];
+// console.log(arr.sayHelloToAjay());
+
+Object.prototype.sayHelloToAjay = function () {
+  return `Hello ${this.name}!!, how are you?`;
+};
+let obj = {
+  name: "AJay",
+  age: 20,
+};
+// console.log(obj.sayHelloToAjay());
