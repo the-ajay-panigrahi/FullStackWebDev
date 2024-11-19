ae204ef9fff0ebfb30e03fb27fc8a3fbece08091
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-11-19 21:15:39 +0530

    data types

---
+++ 05_JAVASCRIPT/02_basics/03_data-types.js
@@ -0,0 +1,43 @@
+/*
+    Data Types :-
+    1. Number
+    2. String
+    3. Boolean
+    4. Undefined
+    5. Null
+    6. Symbol
+    7. BigInt
+    8. Object
+*/
+
+// 1. Number
+let num = 10;
+console.log(num);
+
+// 2. String
+let str = "Hello World";
+console.log(str);
+
+// 3. Boolean
+let bool = true;
+console.log(bool);
+
+// 4. Undefined
+let undef = undefined;
+console.log(undef);
+
+// 5. Null
+let nul = null;
+console.log(nul);
+
+// 6. Symbol
+let sym = Symbol("123");
+console.log(sym);
+
+// 7. BigInt
+let bigNum = 9007199254740991n;
+console.log(bigNum);
+
+// 8. Object
+let obj = { firstName: "Ajay", lastName: "Panigrahi" };
+console.log(obj);
