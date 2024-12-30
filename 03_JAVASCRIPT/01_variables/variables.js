41b7a4cd62b7f8cd173b8ae74568711832a91836
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-12-30 22:30:26 +0530

    variables

---
+++ 03_JAVASCRIPT/01_variables/variables.js
@@ -0,0 +1,13 @@
+var a = 5; // old way
+// console.log(a);
+a = 13;
+// console.log(a);
+
+let b = 33; // new way
+// console.log(b);
+b = 3;
+// console.log(b);
+
+const pi = 3.14; // for constants
+// console.log(pi);
+// pi = 3; // Not allowed
