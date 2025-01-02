02ef0cfa164934461b4f8007e0e7fdb899d6195f
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-01-02 19:22:45 +0530

    loops

---
+++ 03_JAVASCRIPT/05_loops/index.js
@@ -0,0 +1,36 @@
+// for loop
+for (let i = 1; i <= 100; i++) {
+  //   console.log(i);
+}
+
+// while loop
+let i = 1;
+while (i <= 5) {
+  //   console.log(i);
+  i++;
+}
+
+// do while loop
+let k = -23;
+do {
+  //   console.log(k);
+  i++;
+} while (k >= 0);
+
+let obj = {
+  firstName: "Ajay",
+  age: 20,
+  role: "Programmer",
+};
+
+// for in loop
+for (key in obj) {
+  //   console.log(key);
+  //   console.log(obj[key]);
+  //   console.log(key, " : ", obj[key]);
+}
+
+// for of loop
+for (value of "Ajay") {
+  console.log(value);
+}
